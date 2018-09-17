8d228a7d2232b50d9677e14b2dcd7653c92794bb
D7702_10 <d7702_10@d7702-10ui-imac.local>
2018-09-17 12:02:15 +0900

    xml

---
+++ Fruit Parsing03/FruitData.swift
@@ -0,0 +1,14 @@
+//
+//  FruitData.swift
+//  Fruit Parsing03
+//
+//  Created by D7702_10 on 2018. 9. 17..
+//  Copyright © 2018년 ksh. All rights reserved.
+//
+
+import Foundation
+class FruitData {
+    var name = ""
+    var color = ""
+    var cost = ""
+}
